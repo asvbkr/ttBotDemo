2dd7ea044ba7ca82430229147cf4e5f64bfd9dbe
ASV <asv@bk.ru>
2019-05-25 15:08:11 +0300

    -

--- TamTamBot/TamTamBot.py
+++ TamTamBot/TamTamBot.py
@@ -252,9 +252,8 @@ class TamTamBot(object):
     def handle_request_body(self, request_body):
         # type: (bytes) -> None
         if request_body:
-            self.lgz.debug('request_body: %s' % request_body)
+            self.lgz.debug('request body:\n%s\n%s' % (request_body, request_body.decode('utf-8')))
             data = json.loads(request_body)
-            self.lgz.debug('data request_body: %s' % data)
             if data.get('update_type'):
                 incoming_data = self.client.deserialize(RESTResponse(urllib3.HTTPResponse(request_body)), Update.discriminator_value_class_map.get(data.get('update_type')))
                 self.lgz.debug('incoming data:\n type=%s;\n data=%s' % (type(incoming_data), incoming_data))
