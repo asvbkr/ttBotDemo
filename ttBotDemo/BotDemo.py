72aad4a6ad3bb847ede0315c7b399614f085a776
asvbkr <sagalcov@roskazna.ru>
2019-07-09 09:46:18 +0300

    API v. 0.1.7

--- ttBotDemo/BotDemo.py
+++ ttBotDemo/BotDemo.py
@@ -9,6 +9,12 @@ from TamTamBot.utils.lng import set_use_django
 class BotDemo(TamTamBot):
 
     @property
+    def description(self):
+        # type: () -> str
+        return 'Демобот на python. Исходный код - https://github.com/asvbkr/ttBotDemo.\n\n' \
+               'Demo-bot in python. Source code - https://github.com/asvbkr/ttBotDemo.'
+
+    @property
     def token(self):
         # type: () -> str
         token = os.environ.get('TT_BOT_DEMO_API_TOKEN')
